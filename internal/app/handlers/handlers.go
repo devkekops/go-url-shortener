fdd2bf0abfabc93f7e8b6e0ff213c8a70720f703
Dmitriy Tereshin <dm.teryoshin@gmail.com>
2022-04-04 16:11:27 +0300

    fix gzip requests

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -58,7 +58,7 @@ func base62ToBase10(str string) int64 {
 
 func gzipHandle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get(`Content-Encoding`) == `gzip` {
+		if r.Header.Get("Content-Encoding") == "gzip" {
 			gz, err := gzip.NewReader(r.Body)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
@@ -66,7 +66,6 @@ func gzipHandle(next http.Handler) http.Handler {
 			}
 			defer gz.Close()
 			r.Body = gz
-			w.Header().Set("Content-Encoding", "gzip")
 		}
 		next.ServeHTTP(w, r)
 	})
